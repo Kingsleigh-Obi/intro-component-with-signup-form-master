852dc04ef0342f24b6035a8b3db13ae84c67d490
Kingsley Obiagwu <kingsleybgw@gmail.com>
2026-07-18 14:09:14 +0100

    error message feature for the email

---
+++ JS/index.js
@@ -0,0 +1,25 @@
+const inputElements = document.querySelectorAll('input');
+
+inputElements.forEach((inputElement) => {
+  inputElement.addEventListener('input', () => {
+    if(inputElement.value) {
+    inputElement.classList.add('red-border');
+  } else {
+    inputElement.classList.remove('red-border');
+  }
+  });
+});
+
+const emailField = document.getElementById('email');
+const errorMessage = document.querySelector('.error-message');
+
+emailField.addEventListener('input', () => {
+  if(emailField.checkValidity()) {
+    errorMessage.classList.remove('show-error-message');
+  } else {
+    errorMessage.classList.add('show-error-message');
+    errorMessage.textContent = 'Please enter a valid email';
+  }
+});
+
+
